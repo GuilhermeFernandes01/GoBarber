5862f6107aac33d58ce782a091f50dceb0836ea7
Guilherme Fernandes <guilherme.fernandes01@outlook.com>
2020-02-28 20:01:45 -0300

    Add try catch on schedule controller

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -6,31 +6,35 @@ import User from '../models/User';
 
 class ScheduleController {
   async index(req, res) {
-    const isProvider = await User.findOne({
-      where: { id: req.userId, provider: true },
-    });
+    try {
+      const isProvider = await User.findOne({
+        where: { id: req.userId, provider: true },
+      });
 
-    if (!isProvider) {
-      return res
-        .status(401)
-        .json({ error: 'You can only create appointments with providers!' });
-    }
+      if (!isProvider) {
+        return res
+          .status(401)
+          .json({ error: 'You can only create appointments with providers!' });
+      }
 
-    const { date } = req.query;
-    const parsedDate = parseISO(date);
+      const { date } = req.query;
+      const parsedDate = parseISO(date);
 
-    const appointments = await Appointment.findAll({
-      where: {
-        provider_id: req.userId,
-        canceled_at: null,
-        date: {
-          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+      const appointments = await Appointment.findAll({
+        where: {
+          provider_id: req.userId,
+          canceled_at: null,
+          date: {
+            [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+          },
         },
-      },
-      order: ['date'],
-    });
+        order: ['date'],
+      });
 
-    return res.json(appointments);
+      return res.json(appointments);
+    } catch (error) {
+      return res.status(500).json({ message: error.message, error });
+    }
   }
 }
 
