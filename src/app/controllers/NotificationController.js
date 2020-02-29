604c43292e099f7087cc5e5bcab08477e1a92ea9
Guilherme Fernandes <guilherme.fernandes01@outlook.com>
2020-02-28 21:18:29 -0300

    Update read notification

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -21,6 +21,16 @@ class NotificationController {
 
     return res.json(notifications);
   }
+
+  async update(req, res) {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+
+    return res.status(200).json(notification);
+  }
 }
 
 export default new NotificationController();
