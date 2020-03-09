d1596ce22d26ef3a3cba8000eedc1f0ad1fbcf82
Guilherme Fernandes <guilherme.fernandes01@outlook.com>
2020-03-09 14:59:17 -0300

    Add service to send emails

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'b58850a6c98bd4',
+    pass: 'd42172a684e575',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
